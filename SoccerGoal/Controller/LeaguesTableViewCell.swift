5e66e1eac12933ee7eac85f42b19cb7b923f6954
Jose Arguinzzones <45632305+realchema@users.noreply.github.com>
2021-05-20 13:03:40 -0400

    finished leagues tables, and leagues custom  cell

--- SoccerGoal/Controller/LeaguesTableViewCell.swift
+++ SoccerGoal/Controller/LeaguesTableViewCell.swift
@@ -6,21 +6,51 @@
 //
 
 import UIKit
+import SVGKit
 
 class LeaguesTableViewCell: UITableViewCell {
     
     let dataInfoUtility = DataInfoUtility()
     
-
+    @IBOutlet weak var leagueImage: UIImageView!
+    @IBOutlet weak var leagueNameLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
+    
+    
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
-
+    
+    func updateUI(with competitionInfo: Competition, index: Int) {
+      
+        let path = competitionInfo.crestURL
+       
+        print(path)
+        if let url = URL(string: path) {
+            let task = URLSession.shared.dataTask(with: url) { data, response, error in
+                guard let data = data, error == nil else { return }
+                
+                DispatchQueue.main.async { /// execute on main thread
+                    let mySVGImage: SVGKImage = SVGKImage(data: data)
+                    self.leagueImage?.image = mySVGImage.uiImage
+                    self.leagueNameLabel.text = competitionInfo.name
+                }
+            }
+            
+            task.resume()
+        }
+        
+    }
+    
+    func updateUI(with error: String) {
+        leagueImage.image = UIImage(systemName: "exclamationmark.octagon")
+        leagueNameLabel.text = error
+    }
 }
--- SoccerGoal/Controller/LeaguesViewController.swift
+++ SoccerGoal/Controller/LeaguesViewController.swift
@@ -11,37 +11,68 @@ class LeaguesViewController: UIViewController, UITableViewDelegate, UITableViewD
    
     let dataInfoUtility = DataInfoUtility()
     var callStatus: Bool = false
-    var errorStatus: Error? = nil
-    var da = [Competition]()
-    
+    var errorStatus: String = "No leagues found"
+    var leagues = Competition.defaultCompetitions
     
     @IBOutlet weak var leaguesTableView: UITableView!
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        leaguesTableView.delegate = self
+        leaguesTableView.dataSource = self
+       
+        if leagues.isEmpty {
+            callStatus = false
+        }
+        else {
+            callStatus = true
+        }
     }
     
 
-    /*
-    // MARK: - Navigation
+    // MARK: - Table view data source
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-    */
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        var numberOfRow = 1
+               switch tableView {
+                   case leaguesTableView:
+                    numberOfRow = leagues.count
+                    
+                   default:
+                       print("something is wrong")
+                   }
+        return numberOfRow
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = UITableViewCell()
+                switch tableView {
+                    case leaguesTableView:
+                       let cell = tableView.dequeueReusableCell(withIdentifier: "leaguesCell", for: indexPath) as! LeaguesTableViewCell
+                        let league = leagues[indexPath.row]
+                        if callStatus{
+                            cell.updateUI(with: league, index: indexPath.row)
+                        }
+                        else {
+                            cell.updateUI(with: errorStatus)
+                        }
+                        cell.showsReorderControl = true
+                        return cell
+                default:
+                    print("Something is Wrong")
+                }
+                return cell
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        leaguesTableView.reloadData()
     }
 
 }
